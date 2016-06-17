4b11d3cc333830487b6da2d20e38070e853feb82
Emile <emile.swarts123@gmail.com>
2016-06-17 16:00:07 +0100

    Fix spec.

--- test/https_www_redirector/core_test.clj
+++ test/https_www_redirector/core_test.clj
@@ -4,4 +4,4 @@
 
 (deftest ensure-valid-url-test
   (testing "Transform non https, non www urls to include these"
-    (is (= (eval ensure-valid-url) "foo"))))
+    (is (= (ensure-valid-url "") "foo"))))
